25abb9c432b3758fce0834d9a2370a052b67c72e
Jon Ham <23408816+jondwoo@users.noreply.github.com>
2020-11-28 11:45:53 -0800

    add course filter for dashboard

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
 
 import StudentInfo from '../../components/StudentInfo/StudentInfo';
 import CurrentSchedule from '../../components/Tables/CurrentSchedule/CurrentSchedule';
@@ -9,39 +10,48 @@ import MajorRemaining from '../../components/Tables/RemainingCourses/MajorRemain
 import MajorRequirements from '../../components/Tables/Requirements/MajorRequirements/MajorRequirements';
 import GeRemaining from '../../components/Tables/RemainingCourses/GeRemaining/GeRemaining';
 import GeRequirements from '../../components/Tables/Requirements/GeRequirements/GeRequirements';
+import { majorReqCategory } from '../../components/Tables/Requirements/MajorRequirements/CecsReqData';
 
 const Dashboard = () => {
+  const [radioValue, setRadioValue] = useState('ge');
+
+  const handleCourseChange = (value) => {
+    setRadioValue(value);
+  };
+
   return (
     <>
-      <div style={{ height: '2000px' }}>
-        <Row className="mt-5">
-          <Col>
-            <StudentInfo />
-          </Col>
-        </Row>
+      <Row className="mt-5">
+        <Col>
+          <StudentInfo />
+        </Col>
+      </Row>
 
-        <Row className="mt-4">
-          <Col>
-            <CurrentSchedule />
-          </Col>
-        </Row>
-        <Row className="mt-4">
-          <Col>
-            <MajorRemaining />
-          </Col>
-          <Col>
-            <MajorRequirements />
-          </Col>
-        </Row>
-        <Row className="my-5">
-          <Col>
-            <GeRemaining />
-          </Col>
-          <Col>
-            <GeRequirements />
-          </Col>
-        </Row>
-      </div>
+      <Row className="mt-4">
+        <Col>
+          <CurrentSchedule />
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        <Col className="d-flex justify-content-center">
+          <ToggleButtonGroup
+            className="mb-3"
+            type="radio"
+            name="options"
+            defaultValue="ge"
+            onChange={handleCourseChange}
+          >
+            <ToggleButton value="ge">GE</ToggleButton>
+            <ToggleButton value="major">Major</ToggleButton>
+          </ToggleButtonGroup>
+        </Col>
+      </Row>
+      <Row className="mt-3 mb-5">
+        <Col>{radioValue === 'ge' ? <GeRemaining /> : <MajorRemaining />}</Col>
+        <Col>
+          {radioValue === 'ge' ? <GeRequirements /> : <MajorRequirements />}
+        </Col>
+      </Row>
     </>
   );
 };
