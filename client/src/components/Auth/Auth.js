58ad6d6b15108065381cb10dbecdd776e7dc7639
Jon Ham <jondwoo@gmail.com>
2021-02-06 23:31:22 -0800

    refactor to only oauth

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,42 +1,43 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
+// import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { GoogleLogin } from 'react-google-login';
 import { useDispatch } from 'react-redux';
+import { FaGoogle } from 'react-icons/fa';
 
 import './Auth.css';
 
-import Validator from './Validator';
-import { signin, signup } from '../../actions/auth';
+// import Validator from './Validator';
+// import { signin, signup } from '../../actions/auth';
 
-const initialValues = {
-  email: '',
-  password: '',
-  confirmPassword: '',
-};
+// const initialValues = {
+//   email: '',
+//   password: '',
+//   confirmPassword: '',
+// };
 
 const Auth = () => {
-  const [formData, setFormData] = useState(initialValues);
-  const [rememberMe, setRememberMe] = useState(false);
-  const [isSignUp, setIsSignUp] = useState(false);
+  // const [formData, setFormData] = useState(initialValues);
+  // const [rememberMe, setRememberMe] = useState(false);
+  // const [isSignUp, setIsSignUp] = useState(false);
   const history = useHistory();
 
   const dispatch = useDispatch();
 
-  const handleChange = (e, inputPropName) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-    updateValidators(inputPropName, e.target.value);
-  };
+  // const handleChange = (e, inputPropName) => {
+  //   const { name, value } = e.target;
+  //   setFormData({
+  //     ...formData,
+  //     [name]: value,
+  //   });
+  //   updateValidators(inputPropName, e.target.value);
+  // };
 
   // const isFormValid = () => {
   //   let status = true;
@@ -52,62 +53,62 @@ const Auth = () => {
   //   return status;
   // };
 
-  const displayValidationErrors = (fieldName) => {
-    const validator = Validator[fieldName];
-    const result = '';
-    if (validator && !validator.valid) {
-      const errors = validator.errors.map((info, index) => {
-        return (
-          <span className="error" key={index}>
-            * {info}
-          </span>
-        );
-      });
-      return <div className="error text-danger">{errors}</div>;
-    }
-    return result;
-  };
+  // const displayValidationErrors = (fieldName) => {
+  //   const validator = Validator[fieldName];
+  //   const result = '';
+  //   if (validator && !validator.valid) {
+  //     const errors = validator.errors.map((info, index) => {
+  //       return (
+  //         <span className="error" key={index}>
+  //           * {info}
+  //         </span>
+  //       );
+  //     });
+  //     return <div className="error text-danger">{errors}</div>;
+  //   }
+  //   return result;
+  // };
 
-  const resetValidators = () => {
-    Object.keys(Validator).forEach((fieldName) => {
-      Validator[fieldName].errors = [];
-      Validator[fieldName].state = '';
-      Validator[fieldName].valid = false;
-    });
-  };
+  // const resetValidators = () => {
+  //   Object.keys(Validator).forEach((fieldName) => {
+  //     Validator[fieldName].errors = [];
+  //     Validator[fieldName].state = '';
+  //     Validator[fieldName].valid = false;
+  //   });
+  // };
 
-  const updateValidators = (fieldName, value) => {
-    Validator[fieldName].errors = [];
-    Validator[fieldName].state = value;
-    Validator[fieldName].valid = true;
-    Validator[fieldName].rules.forEach((rule) => {
-      if (rule.test instanceof RegExp) {
-        if (!rule.test.test(value)) {
-          Validator[fieldName].errors.push(rule.message);
-          Validator[fieldName].valid = false;
-        }
-      } else if (typeof rule.test === 'function') {
-        if (!rule.test(value)) {
-          Validator[fieldName].errors.push(rule.message);
-          Validator[fieldName].valid = false;
-        }
-      }
-    });
-  };
+  // const updateValidators = (fieldName, value) => {
+  //   Validator[fieldName].errors = [];
+  //   Validator[fieldName].state = value;
+  //   Validator[fieldName].valid = true;
+  //   Validator[fieldName].rules.forEach((rule) => {
+  //     if (rule.test instanceof RegExp) {
+  //       if (!rule.test.test(value)) {
+  //         Validator[fieldName].errors.push(rule.message);
+  //         Validator[fieldName].valid = false;
+  //       }
+  //     } else if (typeof rule.test === 'function') {
+  //       if (!rule.test(value)) {
+  //         Validator[fieldName].errors.push(rule.message);
+  //         Validator[fieldName].valid = false;
+  //       }
+  //     }
+  //   });
+  // };
 
-  const switchMode = () => {
-    setIsSignUp((prevIsSignUp) => !prevIsSignUp);
-  };
+  // const switchMode = () => {
+  //   setIsSignUp((prevIsSignUp) => !prevIsSignUp);
+  // };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
 
-    if (isSignUp) {
-      dispatch(signup(formData, history));
-    } else {
-      dispatch(signin(formData, history));
-    }
-  };
+  //   if (isSignUp) {
+  //     dispatch(signup(formData, history));
+  //   } else {
+  //     dispatch(signin(formData, history));
+  //   }
+  // };
 
   const googleSuccess = async (res) => {
     const result = res?.profileObj;
@@ -136,7 +137,25 @@ const Auth = () => {
         <Col md={9}>
           <Card className="text-center shadow">
             <Card.Body>
-              <Form className="formStyle" onSubmit={handleSubmit}>
+              <Card.Title>Login to Student Academic Dashboard</Card.Title>
+              <GoogleLogin
+                clientId="274923431758-p0ojivpf6if4q9uvoi7ohqguevopkrus.apps.googleusercontent.com"
+                onSuccess={googleSuccess}
+                onFailure={googleFailure}
+                cookiePolicy="single_host_origin"
+                render={(renderProps) => (
+                  <Button
+                    className="mb-3 mt-3"
+                    variant="primary"
+                    onClick={renderProps.onClick}
+                    disabled={renderProps.disabled}
+                  >
+                    <FaGoogle className="mb-1" /> Sign in with Google
+                  </Button>
+                )}
+              />
+
+              {/* <Form className="formStyle" onSubmit={handleSubmit}>
                 <Form.Group>
                   <Form.Label>Email address</Form.Label>
                   <Form.Control
@@ -211,25 +230,6 @@ const Auth = () => {
                 )}
                 <br />
 
-                {isSignUp ? null : (
-                  <GoogleLogin
-                    clientId="86113368677-0bun4qab0haalfob041h38kruulbmvj4.apps.googleusercontent.com"
-                    onSuccess={googleSuccess}
-                    onFailure={googleFailure}
-                    cookiePolicy="single_host_origin"
-                    render={(renderProps) => (
-                      <Button
-                        className="mb-3"
-                        variant="primary"
-                        onClick={renderProps.onClick}
-                        disabled={renderProps.disabled}
-                      >
-                        Google Sign In
-                      </Button>
-                    )}
-                  />
-                )}
-
                 <br />
                 <Link to={'/forgotPassword'}>Forgot Password?</Link>
                 <br />
@@ -238,7 +238,7 @@ const Auth = () => {
                     ? 'Already have an account? Sign in'
                     : 'Not yet registered? Sign up'}
                 </Link>
-              </Form>
+              </Form> */}
             </Card.Body>
           </Card>
         </Col>
