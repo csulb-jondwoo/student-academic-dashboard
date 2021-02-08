c93d19c853fffca48204b4bb074d55cd541c605f
ryan17stehle <34487497+ryan17stehle@users.noreply.github.com>
2021-02-08 12:21:12 -0800

    new student schema placeholder

---
+++ server/models/newStudent.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+// This will take the new student data such as courses completed, major choice, etc.
+const newStudentSchema = mongoose.Schema({
+    
+});
+
+module.exports = mongoose.model('User', userSchema);
