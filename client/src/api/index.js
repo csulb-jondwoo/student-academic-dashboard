a849e02592bb7ed01b178df2259029da3a3d893b
Ryan Stehle <ryan17stehle@yahoo.com>
2021-02-18 03:51:00 -0800

    course api call

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -7,3 +7,4 @@ const API = axios.create({ baseURL: 'http://localhost:5000' });
 export const googleLogin = () => API.get('/auth/google');
 export const googleLogout = () =>
   API.get('/auth/logout', { withCredentials: true });
+export const getCourses = () => API.get('/courses')
