875a9fdf08dbc2efd96999b4a927c59264a8b9d2
Jon <jondwoo@gmail.com>
2021-03-13 09:46:58 -0800

    in progress

--- client/src/components/Tables/CurrentSchedule/CurrentSchedule.js
+++ client/src/components/Tables/CurrentSchedule/CurrentSchedule.js
@@ -8,21 +8,13 @@ import CurrentCourse from './Course/Course';
 import { myContext } from '../../../context/Context';
 
 const CurrentSchedule = () => {
-  const {
-    getCurrentCourses,
-    getCompletedCourses,
-    currentCourses,
-    completedCourses,
-    user,
-  } = useContext(myContext);
+  const { getCurrentCourses, currentCourses, user } = useContext(myContext);
 
   const [columns, setColumns] = useState([
     {
       title: 'Course',
       field: 'course',
-      cellStyle: {
-        whiteSpace: 'nowrap',
-      },
+      width: 1000,
     },
     {
       title: 'Section',
@@ -31,11 +23,16 @@ const CurrentSchedule = () => {
     {
       title: 'Units',
       field: 'units',
-      width: 10,
     },
     {
-      title: 'Time',
-      field: 'time',
+      title: 'Start Time',
+      field: 'startTime',
+      width: 1000,
+    },
+    {
+      title: 'End Time',
+      field: 'endTime',
+      width: 1000,
     },
     {
       title: 'Days',
@@ -47,92 +44,64 @@ const CurrentSchedule = () => {
     },
   ]);
 
-  const [data, setData] = useState([
-    {
-      course: 'CECS 100 - Intro to Technology',
-      section: 3,
-      units: 3,
-      time: '12:00pm - 2:00pm',
-      days: 'Mon / Wed',
-      location: 'Online',
-    },
-    {
-      course: 'CECS 100 - Intro to Technology',
-      section: 3,
-      units: 3,
-      time: '12:00pm - 2:00pm',
-      days: 'Mon / Wed',
-      location: 'Online',
-    },
-    {
-      course: 'CECS 100 - Intro to Technology',
-      section: 3,
-      units: 3,
-      time: '12:00pm - 2:00pm',
-      days: 'Mon / Wed',
-      location: 'Online',
-    },
-    {
-      course: 'CECS 100 - Intro to Technology',
-      section: 3,
-      units: 3,
-      time: '12:00pm - 2:00pm',
-      days: 'Mon / Wed',
-      location: 'Online',
-    },
-    {
-      course: 'CECS 100 - Intro to Technology',
-      section: 3,
-      units: 3,
-      time: '12:00pm - 2:00pm',
-      days: 'Mon / Wed',
-      location: 'Online',
-    },
-    {
-      course: 'CECS 100 - Intro to Technology',
-      section: 3,
-      units: 3,
-      time: '12:00pm - 2:00pm',
-      days: 'Mon / Wed',
-      location: 'Online',
-    },
-    {
-      course: 'CECS 100 - Intro to Technology',
-      section: 3,
-      units: 3,
-      time: '12:00pm - 2:00pm',
-      days: 'Mon / Wed',
-      location: 'Online',
-    },
-    {
-      course: 'CECS 100 - Intro to Technology',
-      section: 3,
-      units: 3,
-      time: '12:00pm - 2:00pm',
-      days: 'Mon / Wed',
-      location: 'Online',
-    },
-  ]);
+  // const [data, setData] = useState([
+  //   {
+  //     course: 'CECS 100 - Intro to Technology',
+  //     section: 3,
+  //     units: 3,
+  //     time: '12:00pm - 2:00pm',
+  //     days: 'Mon / Wed',
+  //     location: 'Online',
+  //   },
+  //   {
+  //     course: 'CECS 100 - Intro to Technology',
+  //     section: 3,
+  //     units: 3,
+  //     time: '12:00pm - 2:00pm',
+  //     days: 'Mon / Wed',
+  //     location: 'Online',
+  //   },
+  // ]);
+  const [data, setData] = useState(currentCourses);
 
   const userID = JSON.parse(user).googleId;
 
   useEffect(() => {
     getCurrentCourses(userID);
-    // getCompletedCourses(userID);
   }, [getCurrentCourses, userID]);
+
+  console.log(currentCourses);
+
+  const formatDate = (date) => {
+    var d = new Date(date);
+    var hh = d.getHours();
+    var m = d.getMinutes();
+    var dd = 'AM';
+    var h = hh;
+    if (h >= 12) {
+      h = hh - 12;
+      dd = 'PM';
+    }
+    if (h === 0) {
+      h = 12;
+    }
+
+    var replacement = h + ':' + m;
+    replacement += ' ' + dd;
+
+    return replacement;
+  };
 
   // accumulate total number of units
   const totalUnits = currentCourses.reduce((sum, obj) => {
     return sum + obj.units;
   }, 0);
 
-  const handleUpdate = (params) => {
-    alert('You want to edit ' + data.length + ' rows');
-  };
+  // const handleUpdate = (params) => {
+  //   alert('You want to edit ' + data.length + ' rows');
+  // };
 
-  const handleDelete = (params) => {
-    alert('You want to delete ' + data.length + ' rows');
-  };
+  const handleDelete = (oldData) => {};
 
   return (
     <MaterialTable
@@ -146,44 +115,37 @@ const CurrentSchedule = () => {
       // detailPanel={(rowData) => {
       //   return <p>test</p>;
       // }}
-      editable={{
-        onRowAdd: (newData) =>
-          new Promise((resolve, reject) => {
-            setTimeout(() => {
-              setData([...data, newData]);
-
-              resolve();
-            }, 1000);
-          }),
-        // onRowUpdate: (newData, oldData) =>
-        //   new Promise((resolve, reject) => {
-        //     setTimeout(() => {
-        //       const dataUpdate = [...data];
-        //       const index = oldData.tableData.id;
-        //       dataUpdate[index] = newData;
-        //       setData([...dataUpdate]);
-
-        //       resolve();
-        //     }, 1000);
-        //   }),
-        // onRowDelete: (oldData) =>
-        //   new Promise((resolve, reject) => {
-        //     setTimeout(() => {
-        //       const dataDelete = [...data];
-        //       const index = oldData.tableData.id;
-        //       dataDelete.splice(index, 1);
-        //       setData([...dataDelete]);
-
-        //       resolve();
-        //     }, 1000);
-        //   }),
-      }}
-      actions={[
+      editable={
         {
-          tooltip: 'Edit All Selected Users',
-          icon: 'edit',
-          onClick: (evt, data) => handleUpdate(evt, data),
-        },
+          // onRowAdd: async (newData) => await setData([...data, newData]),
+          // onRowUpdate: (newData, oldData) =>
+          //   new Promise((resolve, reject) => {
+          //     setTimeout(() => {
+          //       const dataDelete = [...data];
+          //       const index = oldData.tableData.id;
+          //       dataDelete.splice(index, 1);
+          //       setData([...dataDelete]);
+          //       resolve();
+          //     }, 1000);
+          //   }),
+          // onRowDelete: (oldData) =>
+          //   new Promise((resolve, reject) => {
+          //     setTimeout(() => {
+          //       const dataDelete = [...data];
+          //       const index = oldData.tableData.id;
+          //       dataDelete.splice(index, 1);
+          //       setData([...dataDelete]);
+          //       resolve();
+          //     }, 1000);
+          //   }),
+        }
+      }
+      actions={[
+        // {
+        //   tooltip: 'Edit All Selected Users',
+        //   icon: 'edit',
+        //   onClick: (evt, data) => handleUpdate(evt, data),
+        // },
         {
           tooltip: 'Remove All Selected Users',
           icon: 'delete',
