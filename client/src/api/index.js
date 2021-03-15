63616dcd34d0d2943192ff70fac9f9768214a2f5
Jon <jondwoo@gmail.com>
2021-03-14 19:55:40 -0700

    organize by req type

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,9 +2,14 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: 'http://localhost:5000' });
 
+// Login/Logout
 export const googleLogout = () =>
   API.get('/auth/logout', { withCredentials: true });
+
+// Get user
 export const fetchUser = () => API.get('/user', { withCredentials: true });
+
+// Get courses
 export const getCurrentCourses = (userID) =>
   API.get('/course/current', {
     params: {
@@ -17,13 +22,23 @@ export const getCompletedCourses = (userID) =>
       ID: userID,
     },
   });
-//export const getCurrentCourses = (userID) => API.get('/course/current', userID);
-//xport const getCompletedCourses = (userID) => API.get('/course/completed', userID);
+
+// Add courses
 export const addCurrentCourse = (course, config) =>
   API.post('/course/current', course, config);
 export const addCompletedCourse = (course, config) =>
   API.post('/course/completed', course, config);
+
+// Delete courses
 export const deleteCurrentCourse = (id) => API.delete(`/course/${id}`);
 export const deleteCompletedCourse = (id) => API.delete(`/course/${id}`);
+
+// Update courses
+export const updateCurrentCourse = ({ newCourse, oldCourse }, config) =>
+  API.put('/course/current', { newCourse, oldCourse }, config);
+export const updateCompletedCourse = ({ newCourse, oldCourse }, config) =>
+  API.put('/course/completed', { newCourse, oldCourse }, config);
+
+// Upload Transcript
 export const uploadTranscript = (data, userID) =>
   API.post('/course/upload', { data, userID });
