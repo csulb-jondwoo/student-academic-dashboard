ac8a26e6d566d07bcdc160750cca5ac87b49a78f
Jon <jondwoo@gmail.com>
2021-03-15 07:38:23 -0700

    clg transcript data to history page

--- client/src/components/Tables/History/MajorHistory/MajorHistory.js
+++ client/src/components/Tables/History/MajorHistory/MajorHistory.js
@@ -1,12 +1,83 @@
-import React from 'react';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Table from 'react-bootstrap/Table';
 
 import { majorHistoryData } from './MajorHistoryData';
 
 import '../../../../utility/css/table-fixed-height.css';
+import { myContext } from '../../../../context/Context';
+import formatTime from '../../../../utility/formatTime/formatTime';
 
 const MajorHistory = () => {
+  const { user, completedCourses, getCompletedCourses } = useContext(myContext);
+  const [isLoading, setIsLoading] = useState(true);
+  const userID = JSON.parse(user).googleId;
+
+  useEffect(() => {
+    // set state of currentCourses inside context via reducer
+    getCompletedCourses(userID);
+    setIsLoading(false);
+  }, [getCompletedCourses, userID, setIsLoading]);
+
+  const columns = [
+    {
+      title: 'Course',
+      field: 'course',
+      width: 1000,
+    },
+    {
+      title: 'Section',
+      field: 'section',
+    },
+    {
+      title: 'Units',
+      field: 'units',
+    },
+    {
+      title: 'Start',
+      field: 'startTime',
+      cellStyle: {
+        whiteSpace: 'nowrap',
+      },
+      width: 1000,
+    },
+    {
+      title: 'End',
+      field: 'endTime',
+      cellStyle: {
+        whiteSpace: 'nowrap', // history.push('dashboard');
+      },
+      width: 1000,
+    },
+    {
+      title: 'Days',
+      field: 'days',
+    },
+    {
+      title: 'Location',
+      field: 'location',
+    },
+  ];
+
+  const courses = useMemo(
+    () =>
+      completedCourses.map((course) => {
+        return {
+          userID: userID,
+          type: course.type,
+          course: course.dept + ' ' + course.number + ' - ' + course.title,
+          grade: course.grade,
+          units: course.units,
+          designation: course.designation,
+          additionalReq: course.additionalReq,
+          termYear: course.term + ' ' + course.year.toString(),
+        };
+      }),
+    [completedCourses, userID]
+  );
+
+  console.log(courses);
+
   return (
     <>
       <Card className="mt-3">
