c0a5ffa3ee3f86bebc56583e5c18deaae051582e
Jon <jondwoo@gmail.com>
2021-03-15 07:22:29 -0700

    simplify history table

--- client/src/components/Tables/History/GeHistory/GeHistory.js
+++ client/src/components/Tables/History/GeHistory/GeHistory.js
@@ -18,250 +18,28 @@ const GeHistory = () => {
         <Table className="mb-3" striped hover bordered responsive="sm">
           <thead>
             <tr>
-              <th>COMMUNICATION & CRITICAL THINKING</th>
-              <th>Units</th>
-              <th>Term</th>
+              <th>Course</th>
               <th>Grade</th>
-              <th>Requirement Designation</th>
-              <th>Status</th>
+              <th>Units</th>
+              <th>Designation</th>
+              <th>Additional Reqs</th>
+              <th>Term</th>
             </tr>
           </thead>
           <tbody>
             {geHistoryData.map((course, idx) => {
-              if (course.designation.startsWith('A')) {
-                return (
-                  <tr key={idx}>
-                    <td>
-                      {course.course} - {course.title}
-                    </td>
-                    <td>{course.units}</td>
-                    <td>{course.term}</td>
-                    <td>{course.grade}</td>
-                    <td>{course.designation}</td>
-                    <td>{course.status}</td>
-                  </tr>
-                );
-              } else {
-                return null;
-              }
-            })}
-          </tbody>
-          <thead>
-            <tr>
-              <th>SCIENCE & MATH</th>
-              <th>Units</th>
-              <th>Term</th>
-              <th>Grade</th>
-              <th>Requirement Designation</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {geHistoryData.map((course, idx) => {
-              if (course.designation.startsWith('B')) {
-                return (
-                  <tr key={idx}>
-                    <td>
-                      {course.course} - {course.title}
-                    </td>
-                    <td>{course.units}</td>
-                    <td>{course.term}</td>
-                    <td>{course.grade}</td>
-                    <td>{course.designation}</td>
-                    <td>{course.status}</td>
-                  </tr>
-                );
-              } else {
-                return null;
-              }
-            })}
-          </tbody>
-          <thead>
-            <tr>
-              <th>ARTS & HUMANITIES</th>
-              <th>Units</th>
-              <th>Term</th>
-              <th>Grade</th>
-              <th>Requirement Designation</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {geHistoryData.map((course, idx) => {
-              if (course.designation.startsWith('C')) {
-                return (
-                  <tr key={idx}>
-                    <td>
-                      {course.course} - {course.title}
-                    </td>
-                    <td>{course.units}</td>
-                    <td>{course.term}</td>
-                    <td>{course.grade}</td>
-                    <td>{course.designation}</td>
-                    <td>{course.status}</td>
-                  </tr>
-                );
-              } else {
-                return null;
-              }
-            })}
-          </tbody>
-          <thead>
-            <tr>
-              <th>SOCIAL SCIENCES & CITIZENSHIP</th>
-              <th>Units</th>
-              <th>Term</th>
-              <th>Grade</th>
-              <th>Requirement Designation</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {geHistoryData.map((course, idx) => {
-              if (course.designation.startsWith('D')) {
-                return (
-                  <tr key={idx}>
-                    <td>
-                      {course.course} - {course.title}
-                    </td>
-                    <td>{course.units}</td>
-                    <td>{course.term}</td>
-                    <td>{course.grade}</td>
-                    <td>{course.designation}</td>
-                    <td>{course.status}</td>
-                  </tr>
-                );
-              } else {
-                return null;
-              }
-            })}
-          </tbody>
-          <thead>
-            <tr>
-              <th>LIFELONG LEARNING & SELF DEVELOPMENT</th>
-              <th>Units</th>
-              <th>Term</th>
-              <th>Grade</th>
-              <th>Requirement Designation</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {geHistoryData.map((course, idx) => {
-              if (course.designation.startsWith('E')) {
-                return (
-                  <tr key={idx}>
-                    <td>
-                      {course.course} - {course.title}
-                    </td>
-                    <td>{course.units}</td>
-                    <td>{course.term}</td>
-                    <td>{course.grade}</td>
-                    <td>{course.designation}</td>
-                    <td>{course.status}</td>
-                  </tr>
-                );
-              } else {
-                return null;
-              }
-            })}
-          </tbody>
-          <thead>
-            <tr>
-              <th>CAPSTONE</th>
-              <th>Units</th>
-              <th>Term</th>
-              <th>Grade</th>
-              <th>Requirement Designation</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {geHistoryData.map((course, idx) => {
-              if (course.designation.startsWith('F')) {
-                return (
-                  <tr key={idx}>
-                    <td>
-                      {course.course} - {course.title}
-                    </td>
-                    <td>{course.units}</td>
-                    <td>{course.term}</td>
-                    <td>{course.grade}</td>
-                    <td>{course.designation}</td>
-                    <td>{course.status}</td>
-                  </tr>
-                );
-              } else {
-                return null;
-              }
-            })}
-          </tbody>
-          <thead>
-            <tr>
-              <th>ADDITIONAL REQUIREMENTS</th>
-              <th>Units</th>
-              <th>Term</th>
-              <th>Grade</th>
-              <th>Requirement Designation</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <em>GLOBAL ISSUES</em>
-              </td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            {geHistoryData.map((course, idx) => {
-              if (course.additionalReq === 'Global Issues') {
-                return (
-                  <tr key={idx}>
-                    <td>
-                      {course.course} - {course.title}
-                    </td>
-                    <td>{course.units}</td>
-                    <td>{course.term}</td>
-                    <td>{course.grade}</td>
-                    <td>{course.designation}</td>
-                    <td>{course.status}</td>
-                  </tr>
-                );
-              } else {
-                return null;
-              }
-            })}
-            <tr>
-              <td>
-                <em>HUMAN DIVERSITY</em>
-              </td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            {geHistoryData.map((course, idx) => {
-              if (course.additionalReq === 'Human Diversity') {
-                return (
-                  <tr key={idx}>
-                    <td>
-                      {course.course} - {course.title}
-                    </td>
-                    <td>{course.units}</td>
-                    <td>{course.term}</td>
-                    <td>{course.grade}</td>
-                    <td>{course.designation}</td>
-                    <td>{course.status}</td>
-                  </tr>
-                );
-              } else {
-                return null;
-              }
+              return (
+                <tr key={idx}>
+                  <td>
+                    {course.course} - {course.title}
+                  </td>
+                  <td>{course.grade}</td>
+                  <td>{course.units}</td>
+                  <td>{course.designation}</td>
+                  <td>{course.additionalReq}</td>
+                  <td>{course.term}</td>
+                </tr>
+              );
             })}
           </tbody>
         </Table>
