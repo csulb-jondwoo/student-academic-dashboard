31eb302c3faec556802e6ea63d4fff2134d1ff6e
Jon <jondwoo@gmail.com>
2021-03-17 04:50:17 -0700

    return res from context

--- client/src/context/Context.js
+++ client/src/context/Context.js
@@ -56,6 +56,7 @@ export default function Context(props) {
         type: 'GET_CURRENT_COURSES',
         payload: res.data.data,
       });
+      return res;
     } catch (err) {
       dispatch({
         type: 'COURSE_ERROR',
@@ -73,6 +74,7 @@ export default function Context(props) {
         type: 'GET_COMPLETED_COURSES',
         payload: res.data.data,
       });
+      return res;
     } catch (err) {
       dispatch({
         type: 'COURSE_ERROR',
@@ -89,12 +91,13 @@ export default function Context(props) {
       },
     };
     try {
-      await api.deleteCurrentCourse(data, config);
+      const res = await api.deleteCurrentCourse(data, config);
 
       // dispatch({
       //   type: 'DELETE_COURSE',
       //   payload: null,
       // });
+      return res;
     } catch (error) {
       dispatch({
         type: 'COURSE_ERROR',
@@ -105,12 +108,13 @@ export default function Context(props) {
 
   async function deleteCompletedCourse(id) {
     try {
-      await api.deleteCompletedCourse();
+      const res = await api.deleteCompletedCourse();
 
       dispatch({
         type: 'DELETE_COURSE',
         payload: null,
       });
+      return res;
     } catch (error) {
       dispatch({
         type: 'COURSE_ERROR',
@@ -134,6 +138,7 @@ export default function Context(props) {
         type: 'ADD_CURRENT_COURSE',
         payload: res.data.data,
       });
+      return res;
     } catch (error) {
       dispatch({
         type: 'COURSE_ERROR',
@@ -151,11 +156,13 @@ export default function Context(props) {
 
     try {
       const res = await api.addCompletedCourse(course, config);
+      console.log(res);
 
       dispatch({
         type: 'ADD_COMPLETED_COURSE',
         payload: res.data.data,
       });
+      return res;
     } catch (error) {
       dispatch({
         type: 'COURSE_ERROR',
@@ -182,6 +189,7 @@ export default function Context(props) {
         type: 'UPDATE_COURSE',
         payload: res.data.data,
       });
+      return res;
     } catch (error) {
       dispatch({
         type: 'COURSE_ERROR',
@@ -207,6 +215,7 @@ export default function Context(props) {
         type: 'UPDATE_COURSE',
         payload: res.data.data,
       });
+      return res;
     } catch (error) {
       dispatch({
         type: 'COURSE_ERROR',
