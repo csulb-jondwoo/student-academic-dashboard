24fb6c78e17a10dc95a711e138a310027bf75d3f
Jon <jondwoo@gmail.com>
2021-03-17 04:49:29 -0700

    circular confirm button

---
+++ client/src/components/Forms/CompletedCourseForm/CircularIntegration/CircularIntegration.js
@@ -0,0 +1,69 @@
+import React, { useRef, useEffect } from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { green } from '@material-ui/core/colors';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import CheckIcon from '@material-ui/icons/Check';
+import SaveIcon from '@material-ui/icons/Save';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // display: 'flex',
+    alignItems: 'center',
+  },
+  wrapper: {
+    margin: theme.spacing(1),
+    position: 'relative',
+  },
+  buttonSuccess: {
+    backgroundColor: green[500],
+    '&:hover': {
+      backgroundColor: green[700],
+    },
+  },
+  fabProgress: {
+    color: green[500],
+    position: 'absolute',
+    top: -6,
+    left: 339,
+    zIndex: 1,
+  },
+  buttonProgress: {
+    color: green[500],
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: -12,
+    marginLeft: -12,
+  },
+}));
+
+const CircularIntegration = ({ isLoading, success, handleButtonClick }) => {
+  const classes = useStyles();
+
+  const buttonClassname = clsx({
+    [classes.buttonSuccess]: success,
+  });
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.wrapper}>
+        <Fab
+          aria-label="save"
+          color="primary"
+          className={buttonClassname}
+          onClick={handleButtonClick}
+        >
+          {success ? <CheckIcon /> : <SaveIcon />}
+        </Fab>
+        {isLoading && (
+          <CircularProgress size={68} className={classes.fabProgress} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CircularIntegration;
