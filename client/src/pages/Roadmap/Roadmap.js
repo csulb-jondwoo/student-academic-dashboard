8020636bea95be72fef872cc98e243fa28ad9bf4
ryan17stehle <34487497+ryan17stehle@users.noreply.github.com>
2021-03-24 16:56:54 -0700

    Update Roadmap.js

--- client/src/pages/Roadmap/Roadmap.js
+++ client/src/pages/Roadmap/Roadmap.js
@@ -6,6 +6,34 @@ import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import SchoolYear from '../../components/Tables/SchoolYear/SchoolYear'
 
+/*
+Roadmap:
+
+1. if user has no roadmap -> render "create a new roadmap" // else load saved roadmap. 
+This means I must create a "Roadmap" schema to save for the user. The roadmap object would have an array of "terms"
+Each term has the year, the season, and the courses for that term.
+
+Clicking this button renders a "road map" (or term) component with at least a select year, select term, "+ (add course)", then a surrounding Container.
+Save the term, year, courses in state.
+
+2. if user has a saved roadmap -> load saved roadmap into right  for editing. each term in the roadmap would have to be mapped to create a component
+on the Roadmap page. This would be done in the useEffect, making a call to the DB to fetch whether or not the student has a roadmap.
+
+3. Remove CECS Roadmap, remove Add School Year (should be add term at bottom)
+
+4. Should allow user to select from dropdown (fall, winter, spring, summer), then year. 
+
+5. "+" functionality: a modal window perhaps? new page? 
+maybe populate a list of all required CECS courses on the side for the user to pick from. Have a modal window with a list of required cecs courses.
+Clicking a course adds it to the term?
+
+6. 
+
+Additional idea
+Map the cecs data (name & url) to create components with the link and URL for student to choose from in the roadmap.
+
+*/
+
 const Roadmap = () => {
   return (
     <Container>
