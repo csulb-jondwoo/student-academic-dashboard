72e28ad0ce07c9934b73d51820eaa368eeabbe9f
Jon <jondwoo@gmail.com>
2021-03-26 19:54:35 -0700

    disable remaining/requirement data

--- client/src/components/Tables/RemainingCourses/MajorRemaining/MajorRemainingData/majorRemainingData.js
+++ client/src/components/Tables/RemainingCourses/MajorRemaining/MajorRemainingData/majorRemainingData.js
@@ -1,142 +1,142 @@
-import { majorHistoryData } from '../../../History/MajorHistory/MajorHistoryData'
-import { majorReqData } from '../../../Requirements/MajorRequirements/CecsReqData'
+// import { majorHistoryData } from '../../../History/MajorHistory/MajorHistoryData'
+// import { majorReqData } from '../../../Requirements/MajorRequirements/CecsReqData'
 
-const getMajorRemaining = () => {
-  const LOWER_DIV_UNITS = 35
-  const APPROVED_SCIENCE_UNITS = 8
-  const UPPER_DIV_UNITS = 39
-  const WRITING_INTENSIVE_UNITS = 3
-  const CORE_ELECTIVE_UNITS = 6
-  const APPLIED_ELECTIVE_UNITS = 3
+// const getMajorRemaining = () => {
+//   const LOWER_DIV_UNITS = 35
+//   const APPROVED_SCIENCE_UNITS = 8
+//   const UPPER_DIV_UNITS = 39
+//   const WRITING_INTENSIVE_UNITS = 3
+//   const CORE_ELECTIVE_UNITS = 6
+//   const APPLIED_ELECTIVE_UNITS = 3
 
-  let lowerDivUnitCount = 0
-  let approvedScienceUnitCount = 0
-  let upperDivUnitCount = 0
-  let writingIntensiveUnitCount = 0
-  let coreElectiveUnitCount = 0
-  let appliedElectiveUnitCount = 0
+//   let lowerDivUnitCount = 0
+//   let approvedScienceUnitCount = 0
+//   let upperDivUnitCount = 0
+//   let writingIntensiveUnitCount = 0
+//   let coreElectiveUnitCount = 0
+//   let appliedElectiveUnitCount = 0
 
-  for (const course of majorHistoryData) {
-    switch (course.designation) {
-      case 'Lower Div':
-        lowerDivUnitCount += course.units
-        break
-      case 'Physical Science':
-        approvedScienceUnitCount += course.units
-        break
-      case 'Life Science':
-        approvedScienceUnitCount += course.units
-        break
-      case 'Upper Div':
-        upperDivUnitCount += course.units
-        break
-      case 'Writing Intensive':
-        writingIntensiveUnitCount += course.units
-        break
-      case 'Core Elective':
-        coreElectiveUnitCount += course.units
-        break
-      case 'Applied Elective':
-        appliedElectiveUnitCount += course.units
-        break
-      default:
-        break
-    }
-  }
+//   for (const course of majorHistoryData) {
+//     switch (course.designation) {
+//       case 'Lower Div':
+//         lowerDivUnitCount += course.units
+//         break
+//       case 'Physical Science':
+//         approvedScienceUnitCount += course.units
+//         break
+//       case 'Life Science':
+//         approvedScienceUnitCount += course.units
+//         break
+//       case 'Upper Div':
+//         upperDivUnitCount += course.units
+//         break
+//       case 'Writing Intensive':
+//         writingIntensiveUnitCount += course.units
+//         break
+//       case 'Core Elective':
+//         coreElectiveUnitCount += course.units
+//         break
+//       case 'Applied Elective':
+//         appliedElectiveUnitCount += course.units
+//         break
+//       default:
+//         break
+//     }
+//   }
 
-  // check if loop is necessary
-  //    if it is, check if course has been completed
-  //    if it is not, return true to skip that category
-  const compareUnitCount = (reqCourse, completedCourse) => {
-    switch (reqCourse.designation) {
-      case 'Lower Div':
-        if (lowerDivUnitCount !== LOWER_DIV_UNITS) {
-          return (
-            completedCourse.course.split(' ')[1] ===
-            reqCourse.course.split(' ')[1]
-          )
-        }
-        break
-      case 'Physical Science':
-        if (approvedScienceUnitCount !== APPROVED_SCIENCE_UNITS) {
-          return (
-            completedCourse.course.split(' ')[1] ===
-            reqCourse.course.split(' ')[1]
-          )
-        }
-        break
-      case 'Life Science':
-        if (approvedScienceUnitCount !== APPROVED_SCIENCE_UNITS) {
-          return (
-            completedCourse.course.split(' ')[1] ===
-            reqCourse.course.split(' ')[1]
-          )
-        }
-        break
-      case 'Upper Div':
-        if (upperDivUnitCount !== UPPER_DIV_UNITS) {
-          return (
-            completedCourse.course.split(' ')[1] ===
-            reqCourse.course.split(' ')[1]
-          )
-        }
-        break
-      case 'Writing Intensive':
-        if (writingIntensiveUnitCount !== WRITING_INTENSIVE_UNITS) {
-          return (
-            completedCourse.course.split(' ')[1] ===
-            reqCourse.course.split(' ')[1]
-          )
-        }
-        break
-      case 'Core Elective':
-        if (coreElectiveUnitCount !== CORE_ELECTIVE_UNITS) {
-          return (
-            completedCourse.course.split(' ')[1] ===
-            reqCourse.course.split(' ')[1]
-          )
-        }
-        break
-      case 'Applied Elective':
-        if (appliedElectiveUnitCount !== APPLIED_ELECTIVE_UNITS) {
-          return (
-            completedCourse.course.split(' ')[1] ===
-            reqCourse.course.split(' ')[1]
-          )
-        }
-        break
-      default:
-        break
-    }
+//   // check if loop is necessary
+//   //    if it is, check if course has been completed
+//   //    if it is not, return true to skip that category
+//   const compareUnitCount = (reqCourse, completedCourse) => {
+//     switch (reqCourse.designation) {
+//       case 'Lower Div':
+//         if (lowerDivUnitCount !== LOWER_DIV_UNITS) {
+//           return (
+//             completedCourse.course.split(' ')[1] ===
+//             reqCourse.course.split(' ')[1]
+//           )
+//         }
+//         break
+//       case 'Physical Science':
+//         if (approvedScienceUnitCount !== APPROVED_SCIENCE_UNITS) {
+//           return (
+//             completedCourse.course.split(' ')[1] ===
+//             reqCourse.course.split(' ')[1]
+//           )
+//         }
+//         break
+//       case 'Life Science':
+//         if (approvedScienceUnitCount !== APPROVED_SCIENCE_UNITS) {
+//           return (
+//             completedCourse.course.split(' ')[1] ===
+//             reqCourse.course.split(' ')[1]
+//           )
+//         }
+//         break
+//       case 'Upper Div':
+//         if (upperDivUnitCount !== UPPER_DIV_UNITS) {
+//           return (
+//             completedCourse.course.split(' ')[1] ===
+//             reqCourse.course.split(' ')[1]
+//           )
+//         }
+//         break
+//       case 'Writing Intensive':
+//         if (writingIntensiveUnitCount !== WRITING_INTENSIVE_UNITS) {
+//           return (
+//             completedCourse.course.split(' ')[1] ===
+//             reqCourse.course.split(' ')[1]
+//           )
+//         }
+//         break
+//       case 'Core Elective':
+//         if (coreElectiveUnitCount !== CORE_ELECTIVE_UNITS) {
+//           return (
+//             completedCourse.course.split(' ')[1] ===
+//             reqCourse.course.split(' ')[1]
+//           )
+//         }
+//         break
+//       case 'Applied Elective':
+//         if (appliedElectiveUnitCount !== APPLIED_ELECTIVE_UNITS) {
+//           return (
+//             completedCourse.course.split(' ')[1] ===
+//             reqCourse.course.split(' ')[1]
+//           )
+//         }
+//         break
+//       default:
+//         break
+//     }
 
-    return true
-  }
+//     return true
+//   }
 
-  const addToRemaining = (reqCourse) => {
-    return !majorHistoryData.some((completedCourse) =>
-      compareUnitCount(reqCourse, completedCourse),
-    )
-  }
+//   const addToRemaining = (reqCourse) => {
+//     return !majorHistoryData.some((completedCourse) =>
+//       compareUnitCount(reqCourse, completedCourse),
+//     )
+//   }
 
-  const majorRemaining = majorReqData.filter((reqCourse) =>
-    addToRemaining(reqCourse),
-  )
+//   const majorRemaining = majorReqData.filter((reqCourse) =>
+//     addToRemaining(reqCourse),
+//   )
 
-  return {
-    majorRemaining,
-    lowerDivUnitCount,
-    approvedScienceUnitCount,
-    upperDivUnitCount,
-    writingIntensiveUnitCount,
-    coreElectiveUnitCount,
-    appliedElectiveUnitCount,
-    LOWER_DIV_UNITS,
-    APPROVED_SCIENCE_UNITS,
-    UPPER_DIV_UNITS,
-    WRITING_INTENSIVE_UNITS,
-    CORE_ELECTIVE_UNITS,
-    APPLIED_ELECTIVE_UNITS,
-  }
-}
+//   return {
+//     majorRemaining,
+//     lowerDivUnitCount,
+//     approvedScienceUnitCount,
+//     upperDivUnitCount,
+//     writingIntensiveUnitCount,
+//     coreElectiveUnitCount,
+//     appliedElectiveUnitCount,
+//     LOWER_DIV_UNITS,
+//     APPROVED_SCIENCE_UNITS,
+//     UPPER_DIV_UNITS,
+//     WRITING_INTENSIVE_UNITS,
+//     CORE_ELECTIVE_UNITS,
+//     APPLIED_ELECTIVE_UNITS,
+//   }
+// }
 
-export default getMajorRemaining
+// export default getMajorRemaining
--- client/src/components/Tables/Requirements/MajorRequirements/MajorRequirements.js
+++ client/src/components/Tables/Requirements/MajorRequirements/MajorRequirements.js
@@ -1,183 +1,183 @@
-import React from 'react'
-import Card from 'react-bootstrap/Card'
-import Table from 'react-bootstrap/Table'
+// import React from 'react'
+// import Card from 'react-bootstrap/Card'
+// import Table from 'react-bootstrap/Table'
 
-import { majorReqData } from './CecsReqData'
+// import { majorReqData } from './CecsReqData'
 
-import '../../../../utility/css/table-fixed-height.css'
+// import '../../../../utility/css/table-fixed-height.css'
 
-const MajorRequirements = () => {
-  return (
-    <>
-      <div className="shadow-sm">
-        <Card>
-          <Card.Body>
-            <Card.Title>CECS Requirements (2020-2021)</Card.Title>
-          </Card.Body>
-        </Card>
-        <div className="table-wrapper">
-          <Table maxHeight="100px" striped hover bordered responsive="sm">
-            <thead>
-              <tr>
-                <th>Lower Division (Take all of the following)</th>
-                <th>Units</th>
-              </tr>
-            </thead>
-            <tbody>
-              {majorReqData.map((course, idx) => {
-                if (course.designation === 'Lower Div') {
-                  return (
-                    <tr key={idx}>
-                      <td>
-                        {course.course} - {course.title}
-                      </td>
-                      <td>{course.units}</td>
-                    </tr>
-                  )
-                } else {
-                  return null
-                }
-              })}
-            </tbody>
-            <thead>
-              <tr>
-                <th>Approved Science (1 Physical, 1 Life; 8 Units Req.)</th>
-                <th>Units</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th colSpan="2">
-                  <em>PHYSICAL SCIENCE</em>
-                </th>
-              </tr>
-              {majorReqData.map((course, idx) => {
-                if (course.designation === 'Physical Science') {
-                  return (
-                    <tr key={idx}>
-                      <td>
-                        {course.course} - {course.title}
-                      </td>
-                      <td>{course.units}</td>
-                    </tr>
-                  )
-                } else {
-                  return null
-                }
-              })}
-              <tr>
-                <th colSpan="2">
-                  <em>LIFE SCIENCE</em>
-                </th>
-              </tr>
-              {majorReqData.map((course, idx) => {
-                if (course.designation === 'Life Science') {
-                  return (
-                    <tr key={idx}>
-                      <td>
-                        {course.course} - {course.title}
-                      </td>
-                      <td>{course.units}</td>
-                    </tr>
-                  )
-                } else {
-                  return null
-                }
-              })}
-            </tbody>
-            <thead>
-              <tr>
-                <th>Upper Division (Take all of the following)</th>
-                <th>Units</th>
-              </tr>
-            </thead>
-            <tbody>
-              {majorReqData.map((course, idx) => {
-                if (course.designation === 'Upper Div') {
-                  return (
-                    <tr key={idx}>
-                      <td>
-                        {course.course} - {course.title}
-                      </td>
-                      <td>{course.units}</td>
-                    </tr>
-                  )
-                } else {
-                  return null
-                }
-              })}
-            </tbody>
-            <thead>
-              <tr>
-                <th>Writing Intensive (3 Units Req.)</th>
-                <th>Units</th>
-              </tr>
-            </thead>
-            <tbody>
-              {majorReqData.map((course, idx) => {
-                if (course.designation === 'Writing Intensive') {
-                  return (
-                    <tr key={idx}>
-                      <td>
-                        {course.course} - {course.title}
-                      </td>
-                      <td>{course.units}</td>
-                    </tr>
-                  )
-                } else {
-                  return null
-                }
-              })}
-            </tbody>
-            <thead>
-              <tr>
-                <th>Core Elective (6 Units Req.)</th>
-                <th>Units</th>
-              </tr>
-            </thead>
-            <tbody>
-              {majorReqData.map((course, idx) => {
-                if (course.designation === 'Core Elective') {
-                  return (
-                    <tr key={idx}>
-                      <td>
-                        {course.course} - {course.title}
-                      </td>
-                      <td>{course.units}</td>
-                    </tr>
-                  )
-                } else {
-                  return null
-                }
-              })}
-            </tbody>
-            <thead>
-              <tr>
-                <th>Applied Elective (3 Units Req.)</th>
-                <th>Units</th>
-              </tr>
-            </thead>
-            <tbody>
-              {majorReqData.map((course, idx) => {
-                if (course.designation === 'Applied Elective') {
-                  return (
-                    <tr key={idx}>
-                      <td>
-                        {course.course} - {course.title}
-                      </td>
-                      <td>{course.units}</td>
-                    </tr>
-                  )
-                } else {
-                  return null
-                }
-              })}
-            </tbody>
-          </Table>
-        </div>
-      </div>
-    </>
-  )
-}
+// const MajorRequirements = () => {
+//   return (
+//     <>
+//       <div className="shadow-sm">
+//         <Card>
+//           <Card.Body>
+//             <Card.Title>CECS Requirements (2020-2021)</Card.Title>
+//           </Card.Body>
+//         </Card>
+//         <div className="table-wrapper">
+//           <Table maxHeight="100px" striped hover bordered responsive="sm">
+//             <thead>
+//               <tr>
+//                 <th>Lower Division (Take all of the following)</th>
+//                 <th>Units</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {majorReqData.map((course, idx) => {
+//                 if (course.designation === 'Lower Div') {
+//                   return (
+//                     <tr key={idx}>
+//                       <td>
+//                         {course.course} - {course.title}
+//                       </td>
+//                       <td>{course.units}</td>
+//                     </tr>
+//                   )
+//                 } else {
+//                   return null
+//                 }
+//               })}
+//             </tbody>
+//             <thead>
+//               <tr>
+//                 <th>Approved Science (1 Physical, 1 Life; 8 Units Req.)</th>
+//                 <th>Units</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               <tr>
+//                 <th colSpan="2">
+//                   <em>PHYSICAL SCIENCE</em>
+//                 </th>
+//               </tr>
+//               {majorReqData.map((course, idx) => {
+//                 if (course.designation === 'Physical Science') {
+//                   return (
+//                     <tr key={idx}>
+//                       <td>
+//                         {course.course} - {course.title}
+//                       </td>
+//                       <td>{course.units}</td>
+//                     </tr>
+//                   )
+//                 } else {
+//                   return null
+//                 }
+//               })}
+//               <tr>
+//                 <th colSpan="2">
+//                   <em>LIFE SCIENCE</em>
+//                 </th>
+//               </tr>
+//               {majorReqData.map((course, idx) => {
+//                 if (course.designation === 'Life Science') {
+//                   return (
+//                     <tr key={idx}>
+//                       <td>
+//                         {course.course} - {course.title}
+//                       </td>
+//                       <td>{course.units}</td>
+//                     </tr>
+//                   )
+//                 } else {
+//                   return null
+//                 }
+//               })}
+//             </tbody>
+//             <thead>
+//               <tr>
+//                 <th>Upper Division (Take all of the following)</th>
+//                 <th>Units</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {majorReqData.map((course, idx) => {
+//                 if (course.designation === 'Upper Div') {
+//                   return (
+//                     <tr key={idx}>
+//                       <td>
+//                         {course.course} - {course.title}
+//                       </td>
+//                       <td>{course.units}</td>
+//                     </tr>
+//                   )
+//                 } else {
+//                   return null
+//                 }
+//               })}
+//             </tbody>
+//             <thead>
+//               <tr>
+//                 <th>Writing Intensive (3 Units Req.)</th>
+//                 <th>Units</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {majorReqData.map((course, idx) => {
+//                 if (course.designation === 'Writing Intensive') {
+//                   return (
+//                     <tr key={idx}>
+//                       <td>
+//                         {course.course} - {course.title}
+//                       </td>
+//                       <td>{course.units}</td>
+//                     </tr>
+//                   )
+//                 } else {
+//                   return null
+//                 }
+//               })}
+//             </tbody>
+//             <thead>
+//               <tr>
+//                 <th>Core Elective (6 Units Req.)</th>
+//                 <th>Units</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {majorReqData.map((course, idx) => {
+//                 if (course.designation === 'Core Elective') {
+//                   return (
+//                     <tr key={idx}>
+//                       <td>
+//                         {course.course} - {course.title}
+//                       </td>
+//                       <td>{course.units}</td>
+//                     </tr>
+//                   )
+//                 } else {
+//                   return null
+//                 }
+//               })}
+//             </tbody>
+//             <thead>
+//               <tr>
+//                 <th>Applied Elective (3 Units Req.)</th>
+//                 <th>Units</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {majorReqData.map((course, idx) => {
+//                 if (course.designation === 'Applied Elective') {
+//                   return (
+//                     <tr key={idx}>
+//                       <td>
+//                         {course.course} - {course.title}
+//                       </td>
+//                       <td>{course.units}</td>
+//                     </tr>
+//                   )
+//                 } else {
+//                   return null
+//                 }
+//               })}
+//             </tbody>
+//           </Table>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
 
-export default MajorRequirements
+// export default MajorRequirements
